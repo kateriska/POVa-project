f75e87f363e3acfc8439e73a4ea2a122d9d09896
Ondřej Pospíšil <43493457+gathy@users.noreply.github.com>
2021-12-23 12:16:09 +0100

    přidáno IOU překrytí predikcí

--- video_predict.py
+++ video_predict.py
@@ -13,11 +13,11 @@ import cv2
 
 matplotlib.use('TkAgg')
 
-PATH_TO_MODEL_DIR="trained_model/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8"
+PATH_TO_MODEL_DIR = "trained_model/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8"
 
 PATH_TO_CFG = PATH_TO_MODEL_DIR + "/pipeline.config"
 
-PATH_TO_CKPT = PATH_TO_MODEL_DIR #+ "/checkpoint"
+PATH_TO_CKPT = PATH_TO_MODEL_DIR  # + "/checkpoint"
 CHECKPOINT = "ckpt-26"
 
 PATH_TO_LABELS = "annotations/label_map.pbtxt"
@@ -34,6 +34,50 @@ detection_model = model_builder.build(model_config=model_config, is_training=Fal
 ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
 ckpt.restore(os.path.join(PATH_TO_CKPT, CHECKPOINT)).expect_partial()
 
+
+def intersection_over_union(box1,box2):
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[2], box2[2])
+    y2 = min(box1[3], box2[3])
+
+    inter = (x2 - x1) * (y2 - y1)
+
+    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+
+    return inter / float(box1_area + box2_area - inter)
+
+def filter_detection(prev, det,score_t=0.7,iou_t=0.8):
+    if not len(prev):
+        return det
+    prev_boxes, prev_classes, prev_scores = prev
+
+
+    boxes = det['detection_boxes']
+    classes = det['detection_classes']
+    scores = det['detection_scores']
+
+    idx = np.where(scores > score_t)
+    scores = scores[idx]
+    classes = classes[idx]
+    boxes = boxes[idx]
+
+
+    for a in range(boxes.shape[0]):
+        for b in range(prev_boxes.shape[0]):
+            iou = intersection_over_union(boxes[a],prev_boxes[b])
+            if iou > iou_t:
+                if scores[a] < prev_scores[b]:
+                    scores[a] = prev_scores[b]
+                    classes[a] = prev_classes[b]
+
+    det['detection_boxes'] = boxes
+    det['detection_classes'] = classes
+    det['detection_scores'] = scores
+
+    return det
+
 @tf.function
 def detect_fn(image):
     """Detect objects in image."""
@@ -44,21 +88,24 @@ def detect_fn(image):
 
     return detections
 
+
 end_time = time.time()
 elapsed_time = end_time - start_time
 print('Model loaded in {} seconds'.format(elapsed_time))
 
-
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
                                                                     use_display_name=True)
 
-video_file_name = ""
+video_file_name = "test_video.mp4"
 
-videoCapture = cv2.VideoCapture(video_file_name)    
+videoCapture = cv2.VideoCapture(video_file_name)
+videoWriter = cv2.VideoWriter('out_iou_video.mp4',cv2.VideoWriter_fourcc(*'XVID'),12,(1392,512))
+
 if not videoCapture.isOpened():
     print("Error: Unable to open video file for reading", video_file_name)
     exit(-1)
-    
+
+prev_detection = ()
 while videoCapture.isOpened():
     ret, frame = videoCapture.read()
     if not ret:
@@ -66,41 +113,53 @@ while videoCapture.isOpened():
 
     image_np = np.array(frame)
     input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
-    
+
     detections = detect_fn(input_tensor)
+
+
 
     # All outputs are batches tensors.
     # Convert to numpy arrays, and take index [0] to remove the batch dimension.
     # We're only interested in the first num_detections.
     num_detections = int(detections.pop('num_detections'))
-    if num_detections > 0:
-        detections = {key: value[0, :num_detections].numpy()
-                    for key, value in detections.items()}
-        detections['num_detections'] = num_detections
 
-        # detection_classes should be ints.
-        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    detections = filter_detection(prev_detection, detections,score_t=0.3,iou_t=0.1)
+
+
+    if num_detections > 0:
 
         label_id_offset = 1
         image_np_with_detections = image_np.copy()
 
         viz_utils.visualize_boxes_and_labels_on_image_array(
-                image_np_with_detections,
-                detections['detection_boxes'],
-                detections['detection_classes']+label_id_offset,
-                detections['detection_scores'],
-                category_index,
-                use_normalized_coordinates=True,
-                max_boxes_to_draw=20,
-                min_score_thresh=.4,
-                agnostic_mode=False)
-        
-        cv2.imshow(image_np_with_detections)
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes'] + label_id_offset,
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=20,
+            min_score_thresh=.5,
+            agnostic_mode=False)
+
+        cv2.imshow('out',image_np_with_detections)
+        videoWriter.write(image_np_with_detections)
     else:
-        cv2.imshow(image_np)
+        cv2.imshow('out',image_np)
+        videoWriter.write(image_np)
+
+    prev_detection = (detections['detection_boxes'],detections['detection_classes'],detections['detection_scores'])
 
     if cv2.waitKey(10) == 27:
-            break
+        break
 
 cv2.destroyAllWindows()
 videoCapture.release()
+videoWriter.release()
